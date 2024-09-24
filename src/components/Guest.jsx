4291ef8093ad586be1508becd55e04b0c80a9d26
Zier0Code <momoxsana01@gmail.com>
2024-09-24 12:09:23 +0800

    Added Text Small on Mobile

--- src/components/Guest.jsx
+++ src/components/Guest.jsx
@@ -94,7 +94,7 @@ const Guest = () => {
                         <div className="flex flex-col md:w-auto">
                             {
                                 messages.length === 0 && (
-                                    <div className="rounded-full mb-2 text-white/70 self-end">
+                                    <div className="rounded-full mb-2 text-white/70 self-end text-xs sm:text-base">
                                         Welcome to ORCA Bot! 🐋
                                         <hr className='mb-5 mt-1 border-white/50' />
                                         I’m here to answer your inquiries instantly. Whether it’s about admissions, courses, registrar, just ask away! Let’s get started!
@@ -104,9 +104,9 @@ const Guest = () => {
                                 <div className='flex flex-col md:w-auto'>
                                     <div
                                         key={index}
-                                        className={`p-2 mx-2 mt:2  rounded-full ${message.sender === 'user'
-                                            ? 'bg-customColorIput px-4 text-white self-end'
-                                            : ' text-white/70 self-start flex'
+                                        className={` rounded-full ${message.sender === 'user'
+                                            ? 'p-2 mx-2 mt-2 bg-customColorIput px-4 text-white self-end'
+                                            : 'pr-2 py-2 mt-2 text-white/70 self-start flex'
                                             }`}
                                     >
                                         {message.sender === 'bot' && (
@@ -116,7 +116,7 @@ const Guest = () => {
                                                 className="size-6 md:mr-2 rounded-full"
                                             />
                                         )}
-                                        <p className='ml-2 text-xs sm:text-base'>
+                                        <p className='ml-2 text-sm sm:text-base'>
                                             <NextLine message={message.text} />
                                         </p>
                                     </div>
@@ -125,13 +125,13 @@ const Guest = () => {
                                             <div className="text-xs text-gray-500 ml-2 flex mr-4 justify-end">Time here</div>
                                         )}
                                         {message.sender === 'bot' && (
-                                            <div className="text-xs text-gray-500 flex ml-10 md:ml-14 justify-start">Time here</div>
+                                            <div className="text-xs text-gray-500 flex ml-8 md:ml-10 justify-start">Time here</div>
                                         )}
                                     </div>
                                 </div>
                             ))}
                             {isTyping && (
-                                <div className="p-2 m-2 text-white/70 self-start flex text-xs sm:text-base">
+                                <div className="pr-2 py-2 mt-2 text-white/70 self-start flex text-sm sm:text-base">
                                     <img
                                         src={orca}
                                         alt="Bot"
@@ -149,7 +149,7 @@ const Guest = () => {
                             <input
                                 autoFocus
                                 type="text"
-                                className="pl-9 flex-grow p-2 border text-xs  md:text-md dark:text-white bg-customColorIput border-black/50 focus:outline-none focus:border-white/20 rounded-full"
+                                className="pl-9 flex-grow p-2 border text-sm  md:text-md dark:text-white bg-customColorIput border-black/50 focus:outline-none focus:border-white/20 rounded-full"
                                 placeholder="Type your inquiries here..."
                                 onKeyDown={(e) => {
                                     if (e.key === "Enter" && !isTyping) {
