7c2c5681a6cb70cc74f7a8efebc617b31ffee91d
Zier0Code <momoxsana01@gmail.com>
2024-09-24 16:06:20 +0800

    Fixed Icon and NavBar Customer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import orca from '../assets/images/Orca Landing Customer Logo.png'
-import profile from '../assets/images/profile.jpg'
+import profile from '../assets/images/no user.jpg'
 import Down from '../assets/svgs/Down';
 import Up from '../assets/svgs/Up';
 import LoginDropdown from './LoginDropdown';
@@ -31,6 +31,7 @@ const Navbar = ({ isTyping, setMessages }) => {
 
     const onLogOut = () => {
         removeCookie("customer_authToken", null)
+        setIsUserDropdownOpen(!isUserDropdownOpen);
         dispatch(logout())
     }
     return (
@@ -39,38 +40,68 @@ const Navbar = ({ isTyping, setMessages }) => {
                 customer ? (
                     <nav className='fixed top-0 left-0 w-full bg-inherit shadow-md z-50'>
                         <div className='h-[50px] flex items-center md:pl-36 pl-8 md:justify-between'>
-                            <div className="flex items-center">
-                                <button onClick={toggleDropdown} className='flex items-center'>
-                                    <div className='flex-shrink-0 flex items-center'>
-                                        <img className="h-8 w-auto" src={orca} alt="Logo" />
-                                        <div className='mx-2 flex'>
-                                            <h1 className='font-lemon mt-1 text-white'>ORCA</h1>
+                            <div className="flex items-center justify-between w-full">
+                                <div className='flex'>
+                                    <button onClick={toggleDropdown} className='flex items-center'>
+                                        <div className='flex-shrink-0 flex items-center'>
+                                            <img className="h-8 w-auto" src={orca} alt="Logo" />
+                                            <div className='mx-2 flex'>
+                                                <h1 className='font-lemon mt-1 text-white'>ORCA</h1>
+                                            </div>
                                         </div>
-                                    </div>
+                                    </button>
+                                    {
+                                        !isDropdownOpen && (
+                                            <div className="absolute mt-4 left-2 md:left-40 bg-[#303030] rounded-lg shadow-lg w-48 min-w-[154px] min-h-[168] cursor-default top-10 ">
+                                                <div className='flex justify-start py-6 px-4 text-white font-medium'>
+                                                    Orca Version 1.0
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        !isTyping && (
+                                            <button
+                                                className="p-2 bg-inherit text-white/50 hover:text-white rounded-md hover:bg-black/20 hover:font-medium ml-2"
+                                                onClick={() => setMessages([])}
+                                            >
+                                                <span className=''>New Chat</span>
+                                            </button>
+                                        )
+                                    }
+                                </div>
+                                <button className='mr-5 sm:mr-20 md:mr-40' onClick={toggleUserDropdown}>
+                                    <img className="h-8 w-8 rounded-full" src={profile} alt="Profile" />
                                 </button>
+
                                 {
-                                    !isDropdownOpen && (
-                                        <div className="absolute mt-4 left-2 md:left-40 bg-[#303030] rounded-lg shadow-lg w-48 min-w-[154px] min-h-[168] cursor-default top-10 ">
-                                            <div className='flex justify-start py-6 px-4 text-white font-medium'>
-                                                Orca Version 1.0
+                                    !isUserDropdownOpen && (
+
+                                        <div className="absolute mt-4 right-2 md:right-10 bg-[#303030] rounded-lg shadow-lg w-48 min-w-[154px] min-h-[168] cursor-default top-10 ">
+                                            <div className='flex justify-start py-6 px-4 text-white'>
+                                                <ul className='w-full'>
+                                                    <li>
+                                                        <button className='hover:bg-black/70 w-full p-2 flex justify-start'>
+                                                            <SettingsIcon />
+                                                            <p className='ml-2 '>Settings</p>
+                                                        </button>
+                                                    </li>
+                                                    <li>
+                                                        <button onClick={onLogOut} className='hover:bg-black/70 w-full p-2 flex justify-start'>
+                                                            <LogoutIcon />
+                                                            <p className='ml-2'>
+                                                                Log out
+                                                            </p>
+                                                        </button>
+                                                    </li>
+                                                </ul>
                                             </div>
                                         </div>
                                     )
                                 }
-                                {
-                                    !isTyping && (
-                                        <button
-                                            className="p-2 bg-inherit text-white/50 hover:text-white rounded-md hover:bg-black/20 hover:font-medium ml-2"
-                                            onClick={() => setMessages([])}
-                                        >
-                                            <span className=''>New Chat</span>
-                                        </button>
-                                    )
-                                }
-
                             </div>
                         </div>
-                    </nav>
+                    </nav >
                 ) : (
                     <nav className='fixed top-0 left-0 w-full bg-inherit shadow-md z-50'>
                         <div className='h-[50px] flex items-center md:pl-36 pl-8 md:justify-between'>
