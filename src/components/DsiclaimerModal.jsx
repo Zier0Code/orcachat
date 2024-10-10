2252406d0957723f3cd045b123b66ec6a3f6193d
Zier0Code <momoxsana01@gmail.com>
2024-10-10 13:21:16 +0800

    Fix responsivenes sof disclaimer

--- src/components/DsiclaimerModal.jsx
+++ src/components/DsiclaimerModal.jsx
@@ -4,13 +4,13 @@ const DisclaimerModal = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed inset-0 flex items-center justify-center z-50 text-xs sm:text-sm">
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg mx-auto z-10">
                 <div className="mb-4">
-                    <span className="text-red-600 font-bold animate-pulse text-2xl">Disclaimer:</span>
+                    <span className="text-red-600 font-bold animate-pulse text-xl sm:text-2xl">Disclaimer:</span>
                 </div>
-                <div className="text-sm text-gray-700">
+                <div className="text-xs sm:text-sm text-gray-700">
                     This resource is provided for <span className='font-bold'>informational purposes only</span> and has been trained on publicly available material. <br /> <br />
                     This chatbot is not a substitute for professional advice of any kind. Your use is governed by the Terms of Service. <br /> <br />
                     <span className='font-bold text-customBlue'>Do not share personal information</span> such as your social security number, bank information, or credit card numbers when interacting with the bot. Chats are retained for (24 months). <br /> <br />
