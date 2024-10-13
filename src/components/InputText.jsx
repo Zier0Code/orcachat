aeaba9574a2d0ff6474212ac6c0100877168b1ef
Zier0Code <momoxsana01@gmail.com>
2024-10-14 00:23:31 +0800

    fix button padding

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -9,11 +9,11 @@ const InputText = ({ handleSendMessage, isTyping }) => {
     return (
         <>
             <div className="fixed bottom-0 w-full p-4 shadow-lg pb-4 bg-customBGWhite dark:bg-customBGDark">
-                <div className="pr-6 md:pr-0 max-w-2xl mx-auto relative">
+                <div className=" md:pr-0 max-w-2xl mx-auto relative">
                     <input
                         autoFocus
                         type="text"
-                        className="shadow-md dark:text-white shadow-gray-500 dark:shadow-none pl-4 pr-9 sm:pr-12 md:pl-9 pr- flex-grow p-2 text-sm mb-2 md:text-mddark:text-white focus:outline-none border dark:border-none focus:border-customBlue rounded-full dark:bg-customColorInput w-full"
+                        className="shadow-md dark:text-white shadow-gray-500 dark:shadow-none pl-4 md:pl-9 flex-grow p-2 text-sm mb-2 md:text-mddark:text-white focus:outline-none border dark:border-none focus:border-customBlue rounded-full dark:bg-customColorInput w-full"
                         placeholder="Type your inquiries here..."
                         onKeyDown={(e) => {
                             if (e.key === "Enter" && !isTyping) {
@@ -24,7 +24,7 @@ const InputText = ({ handleSendMessage, isTyping }) => {
                     />
 
                     <div
-                        className={`absolute right-2 top-1/2 transform -translate-y-1/2 pr-7 md:pr-3 pb-3 text-white hover:bg-customColorIput/50 rounded-full
+                        className={`absolute right-2 top-1/2 transform -translate-y-1/2 md:pr-3 pb-3 text-white hover:bg-customColorIput/50 rounded-full
                             ${isTyping ? 'cursor-not-allowed' : ''}`}
                     >
                         {isTyping ?
