9dc180f8069936e4929ad2996b80be83f09fcb60
Zier0Code <momoxsana01@gmail.com>
2024-12-05 08:44:41 +0800

    change

--- src/api/configuration.js
+++ src/api/configuration.js
@@ -3,8 +3,8 @@
 export const url = "https://api.avila.aesirasdf.com/api" // 2nd API URL
 
 // CHATBOT API URL
-export const py_url = "https://rare-daring-vulture.ngrok-free.app/predict"
-// export const py_url = "https://splendid-nearly-tick.ngrok-free.app/predict"
+// export const py_url = "https://rare-daring-vulture.ngrok-free.app/predict"
+export const py_url = "https://splendid-nearly-tick.ngrok-free.app/predict"
 
 export const bot_url = "https://orcachat.vercel.app/"
 
